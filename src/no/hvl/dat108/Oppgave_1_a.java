a62efcc1939557a550995550b2ebb9e9fcfafe55
676627 <142162653+676627@users.noreply.github.com>
2024-08-26 21:09:56 +0200

    Add oppg 1a and 1b solution

--- src/no/hvl/dat108/Oppgave_1_a.java
+++ src/no/hvl/dat108/Oppgave_1_a.java
@@ -1,13 +1,14 @@
 package no.hvl.dat108;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Oppgave_1_a {
 
 	public static void main(String[] args) {
 		List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
-
+		Collections.sort(listen, (s1, s2) -> Integer.valueOf(s1).compareTo(Integer.valueOf(s2)));
+		System.out.println(listen);
 	}
-
 }
--- src/no/hvl/dat108/Oppgave_1_b.java
+++ src/no/hvl/dat108/Oppgave_1_b.java
@@ -1,9 +1,25 @@
 package no.hvl.dat108;
 
+import java.util.function.BiFunction;
+
 public class Oppgave_1_b {
 
 	public static void main(String[] args) {
-		
+		// i)
+		BiFunction<Integer, Integer, Integer> summerFunksjon = (a, b) -> a + b;
+		System.out.println(beregn(12, 13, summerFunksjon));
+
+		// ii)
+		BiFunction<Integer, Integer, Integer> maksFunksjon = (a, b) -> Math.max(a, b);
+		System.out.println(beregn(-5, 3, maksFunksjon));
+
+		// iii)
+		BiFunction<Integer, Integer, Integer> differanseFunksjon = (a, b) -> Math.abs(a - b);
+		System.out.println(beregn(54, 45, differanseFunksjon));
+
 	}
-	
+
+	public static int beregn(int a, int b, BiFunction<Integer, Integer, Integer> bf) {
+		return bf.apply(a, b);
+	}
 }
