3e1b68a175351c5f02106521a9868a0a93ea3666
Simen Strømsnes <simen@simsine.no>
2024-08-29 11:06:02 +0200

    cool

--- src/no/hvl/dat108/oppgaver/Oppgave_3.java
+++ src/no/hvl/dat108/oppgaver/Oppgave_3.java
@@ -1,8 +1,9 @@
 package no.hvl.dat108.oppgaver;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
 
 import no.hvl.dat108.andre.Ansatt;
 import no.hvl.dat108.andre.Kjonn;
@@ -27,7 +28,7 @@ public class Oppgave_3 {
 		System.out.println("b) Antall kvinner: " + antallKvinner);
 		
 		// c)
-		int totalLonn = ansatte.stream().map(t -> t.aarslonn).reduce(0, (a,b) -> a+b);
+		int totalLonn = ansatte.stream().map(t -> t.aarslonn).reduce(0, Integer::sum);
 		double gjennomsnittligLonn = (double) totalLonn / ansatte.size();
 		System.out.println("c) Gjennomsnittlig lønn" + gjennomsnittligLonn);
 		
@@ -46,24 +47,21 @@ public class Oppgave_3 {
 		
 		// f)
 		System.out.println("f) Alle ansatte: ");
-		ansatte.stream().forEach(t -> System.out.println(t.toString()));
+		ansatte.stream().forEach(System.out::println);
 		
 		// g)
 		System.out.println("g) Ansatte med lavest lønn: ");
-		Ansatt minstLonnet = ansatte.stream().min((o1, o2) -> Integer.compare(o1.aarslonn, o2.aarslonn)).orElse(null);
+		Ansatt minstLonnet = ansatte.stream().min((o1, o2) -> Integer.compare(o1.aarslonn, o2.aarslonn)).orElseThrow();
 		List<Ansatt> minstLønnede = ansatte.stream().filter(t -> t.aarslonn == minstLonnet.aarslonn).toList();
 		System.out.println(minstLønnede);
 		
 		// h)
-		List<Integer> tall = new ArrayList<Integer>();
-		for (int i=1;i<=1000;i++) {
-			tall.add(i);
-		}
+		IntStream tallStroem = IntStream.rangeClosed(1, 1000);
 		
-		Predicate<Integer> deleligMed3 = t -> t % 3 == 0;
-		Predicate<Integer> deleligMed5 = t -> t % 5 == 0;
+		IntPredicate deleligMed3 = t -> t % 3 == 0;
+		IntPredicate deleligMed5 = t -> t % 5 == 0;
 		
-		int sum = tall.stream().filter(deleligMed3.or(deleligMed5)).reduce(0, Integer::sum);
+		int sum = tallStroem.filter(deleligMed3.or(deleligMed5)).sum();
 		System.out.println("h) Sum av tall mellom 1-1000 delelig på 3 eller 5: " + sum);
 	}
 }
