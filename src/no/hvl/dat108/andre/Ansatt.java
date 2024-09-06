d624b761b00e98bd1e6bb90ac3461ddea59404c3
676627 <142162653+676627@users.noreply.github.com>
2024-09-06 14:32:31 +0200

    Add oppg2, update ansatt and oppg3 to using setters/getters

--- src/no/hvl/dat108/andre/Ansatt.java
+++ src/no/hvl/dat108/andre/Ansatt.java
@@ -1,22 +1,63 @@
 package no.hvl.dat108.andre;
 
 public class Ansatt {
-	public String fornavn;
-	public String etternavn;
-	public Kjonn kjonn;
-	public String stilling;
-	public int aarslonn;
+	private String fornavn;
+	private String etternavn;
+	private Kjonn kjonn;
+	private String stilling;
+	private int aarslonn;
+
+	public Ansatt(String fornavn, String etternavn, Kjonn kjonn, String stilling, int aarslonn) {
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.kjonn = kjonn;
+		this.stilling = stilling;
+		this.aarslonn = aarslonn;
+	}
+
+	public String getFornavn() {
+		return fornavn;
+	}
+
+	public void setFornavn(String fornavn) {
+		this.fornavn = fornavn;
+	}
+
+	public String getEtternavn() {
+		return etternavn;
+	}
+
+	public void setEtternavn(String etternavn) {
+		this.etternavn = etternavn;
+	}
+
+	public String getStilling() {
+		return stilling;
+	}
+
+	public void setStilling(String stilling) {
+		this.stilling = stilling;
+	}
+
+	public Kjonn getKjonn() {
+		return kjonn;
+	}
+
+	public void setKjonn(Kjonn kjonn) {
+		this.kjonn = kjonn;
+	}
+
 	@Override
 	public String toString() {
 		return "Ansatt [fornavn=" + fornavn + ", etternavn=" + etternavn + ", kjonn=" + kjonn + ", stilling=" + stilling
 				+ ", aarslonn=" + aarslonn + "]";
 	}
-	public Ansatt(String fornavn, String etternavn, Kjonn kjonn, String stilling, int aarslonn) {
-		super();
-		this.fornavn = fornavn;
-		this.etternavn = etternavn;
-		this.kjonn = kjonn;
-		this.stilling = stilling;
+
+	public int getAarslonn() {
+		return aarslonn;
+	}
+
+	public void setAarslonn(int aarslonn) {
 		this.aarslonn = aarslonn;
 	}
 }
--- src/no/hvl/dat108/oppgaver/Oppgave_3.java
+++ src/no/hvl/dat108/oppgaver/Oppgave_3.java
@@ -1,6 +1,5 @@
 package no.hvl.dat108.oppgaver;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
@@ -20,29 +19,29 @@ public class Oppgave_3 {
 		);
 		
 		// a)
-		List<String> etternavn = ansatte.stream().map(t -> t.etternavn).toList();
+		List<String> etternavn = ansatte.stream().map(t -> t.getEtternavn()).toList();
 		System.out.println("a) Etternavn: " + etternavn);
 		
 		// b)
-		long antallKvinner = ansatte.stream().filter(t -> t.kjonn == Kjonn.KVINNE).count();
+		long antallKvinner = ansatte.stream().filter(t -> t.getKjonn() == Kjonn.KVINNE).count();
 		System.out.println("b) Antall kvinner: " + antallKvinner);
 		
 		// c)
-		int totalLonn = ansatte.stream().map(t -> t.aarslonn).reduce(0, Integer::sum);
+		int totalLonn = ansatte.stream().map(t -> t.getAarslonn()).reduce(0, Integer::sum);
 		double gjennomsnittligLonn = (double) totalLonn / ansatte.size();
 		System.out.println("c) Gjennomsnittlig lønn" + gjennomsnittligLonn);
 		
 		// d)
 		ansatte.stream().forEach(t -> {
-			if (t.stilling.toLowerCase().contains("sjef")) {
-				t.aarslonn *= 1.07;
+			if (t.getStilling().toLowerCase().contains("sjef")) {
+				t.setAarslonn(t.getAarslonn() + (t.getAarslonn() * 7 / 100)) ;
 			}
 		});
 		System.out.println("d) Alle ansatte: ");
 		Oppgave_2.skrivUt(ansatte);
-		
+				
 		// e)
-		boolean noenOver800_000 = ansatte.stream().anyMatch(t -> t.aarslonn > 800_000);
+		boolean noenOver800_000 = ansatte.stream().anyMatch(t -> t.getAarslonn() > 800_000);
 		System.out.println("e) Noen tjener over 800_000: " + noenOver800_000);
 		
 		// f)
@@ -51,8 +50,8 @@ public class Oppgave_3 {
 		
 		// g)
 		System.out.println("g) Ansatte med lavest lønn: ");
-		Ansatt minstLonnet = ansatte.stream().min((o1, o2) -> Integer.compare(o1.aarslonn, o2.aarslonn)).orElseThrow();
-		List<Ansatt> minstLønnede = ansatte.stream().filter(t -> t.aarslonn == minstLonnet.aarslonn).toList();
+		Ansatt minstLonnet = ansatte.stream().min((o1, o2) -> Integer.compare(o1.getAarslonn(), o2.getAarslonn())).orElseThrow();
+		List<Ansatt> minstLønnede = ansatte.stream().filter(t -> t.getAarslonn() == minstLonnet.getAarslonn()).toList();
 		System.out.println(minstLønnede);
 		
 		// h)
